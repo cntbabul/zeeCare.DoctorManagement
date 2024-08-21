e8f85891b04f3201b811813b5695dcfaa9995976
MD BABUL MIAH <56152131+cntbabul@users.noreply.github.com>
2024-08-21 16:48:23 +0530

    Update App.jsx url update

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get("/api/v1/user/admin/me", {
+        const response = await axios.get(""http://localhost:4000/api/v1/user/admin/me", {
           withCredentials: true,
         });
         setIsAuthenticated(true);
